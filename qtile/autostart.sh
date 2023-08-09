b208d0809deb1a8a9c08c2d2efbc12462969fe70
Cosheimil <a.varvus@ya.ru>
2023-08-09 13:21:56 +0300

    new programms at start

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 kbdd &
 xrandr --output DP-1 --mode 1920x1080 --rate 120.00 &
-picom -b &
+picom -b -c &
 nitrogen --set-zoom-fill --random /home/artem/Изображения/wallpapers/Gruv/ --save &
+udevil &
 
