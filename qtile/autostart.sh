b0c033f855b412143caa7fc72392bda9889fc942
Cosheimil <a.varvus@ya.ru>
2023-10-17 12:16:46 +0300

    new autostart

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 kbdd &
-xrandr --rate 120 &
-nitrogen --set-zoom-fill --random /home/artem/Pictures/wallpapers/Gruv/ --save &
+xrandr --rate 165
+nitrogen --set-zoom-fill --random /home/artem/Pictures/wallpapers/Gruv/ --save
 udevil &
+xclip & 
+nm-applet &
 
